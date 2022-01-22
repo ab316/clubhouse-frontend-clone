5c099ff153c1c9a9f70c71d93130a1a1d21c009a
Abdullah Baig <abdullahbaig456@gmail.com>
2022-01-22 20:26:17 +0100

    Welcome page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import PlanLayout from './pages/layouts/PlanLayout';
+import Welcome from './pages/Welcome';
+
+function App() {
+  return (
+    <PlanLayout>
+      <Welcome />
+    </PlanLayout>
+  );
+}
+
+export default App;
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,23 @@
+import style from '../styles/welcome.module.css';
+
+export default function Welcome() {
+  return (
+    <div className={style.WelcomeContainer}>
+      <h1>Welcome!</h1>
+      <div className={style.WelcomeInfo}>
+        <p>
+          We're working hard to get clubhouse ready for everyone! While we wrap
+          up the finishing touches, we're adding people gradually to make sure
+          nothing break
+        </p>
+        <p>
+          Anyone can join with an invite from an existing user - or reserve your
+          username and we'll text you if you have a friend on the app who can
+          let you in. We are so grateful you're here and can't wait to have you
+          join!
+        </p>
+        <p>Paul, Rohan & the Clubhouse team</p>
+      </div>
+    </div>
+  );
+}

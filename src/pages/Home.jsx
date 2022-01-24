3ffc6724d1a7ba68d29b3b5b9a1abd82dd3fa3fa
Abdullah Baig <abdullahbaig456@gmail.com>
2022-01-24 21:08:04 +0100

    Bottom menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import DailyInfoCard from '../components/DailyInfoCard';
+import {AiOutlinePlus} from 'react-icons/ai';
+import {BsGrid3X3GapFill} from 'react-icons/bs';
 import Header from '../components/Header';
+import DailyInfoCard from '../components/DailyInfoCard';
 import RoomInfoCard from '../components/RoomInfoCard';
 import style from '../styles/home.module.css';
 
@@ -11,6 +13,15 @@ const Home = () => {
       <div className={style.homeContainer}>
         <DailyInfoCard />
         <RoomInfoCard />
+      </div>
+      <div className={style.actionBtn}>
+        <button>
+          <AiOutlinePlus className="me-2" />
+          Start a room
+        </button>
+        <button>
+          <BsGrid3X3GapFill />
+        </button>
       </div>
     </>
   );
